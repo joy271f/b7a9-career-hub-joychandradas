7a62622ecaa6c7c53b3e2693b98692bfd0ba7197
joychandradas <joychandradas007@gmail.com>
2023-04-11 20:43:17 +0600

    Header Section Done

--- src/components/Homes/Homes.jsx
+++ src/components/Homes/Homes.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import './Homes.css'
+import companyLogo from '../../assets/All_Images/man.png';
+import JobCategory from '../JobCategory/JobCategory';
 
 const Homes = () => {
     return (
-        <div>
-            <h1>home</h1>
+        <>
+        <div className='home-container mt-4 row ps-4'>
+            <div className='home-text cols col-md-6'>
+                <h3 className='home-title'>One Step <br /> Closer To Your <br /> <span className='title-span'>Dream Job</span></h3>
+                <p className='home-description'>A successful interview depends on various factors, from what steps you take to prepare to how you conduct yourself in the meeting. You may have multiple rounds of interviews before receiving a job offer...</p>
+                <botton className="btn btn-primary">Get Started</botton>
+            </div>
+            <div className='cols col-md-6'>
+                <img className='home-image img-fluid w-75' src={companyLogo} alt=""></img>
+            </div>
         </div>
+        <JobCategory></JobCategory>
+        </>
     );
 };
 
---
+++ src/components/JobCategory/JobCategory.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const JobCategory = () => {
+    return (
+        <>
+            <div className='mt-5 text-center'>
+                <h3>Job Category List</h3>
+                <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+            <div>
+                <img src="" alt="" />
+                <h5>Account & Finance</h5>
+                <p>300 Jobs Available</p>
+            </div>
+        </>
+    );
+};
+
+export default JobCategory;
